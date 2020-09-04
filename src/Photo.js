a8613a54f4c0b9a1ca535159eb4725e6138b2a74
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-04 21:51:36 +0100

    added html markup

---
+++ src/Photo.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Photo.css'
+const Photo = () => {
+    return (
+        <div>
+            <div className="w">
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="i"></div>
+                <div className="h">
+                    <h1>Hari</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Photo;
